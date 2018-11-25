62d250d6c11f317ae4b64d8ee5bc9ad4ba38f2ad
saburabrina <sabrinascosme@gmail.com>
2018-11-25 14:14:11 -0300

    front

--- processo.js
+++ processo.js
@@ -2,27 +2,38 @@ var processos = [];
 var pids = -1;
 var tempo = 0;
 turnaroundmedio = 0;
+escalonamento = 0;
 
-var Processo = function (pid, tempochegada, tempoexecucao, deadline) {
+var Processo = function (pid, tempochegada, tempoexecucao, deadline, prioridade) {
 	this.pid = pid,
 	this.tempochegada = tempochegada,
 	this.tempoexecucao = tempoexecucao,
 	this.deadline = deadline
 	this.turnaround = 0;
+	this.prioridade = prioridade;
 };
 
 function criarprocesso(){
 	tempochegada = document.getElementById('chegada').value - 0;
 	tempoexecucao = document.getElementById('execucao').value - 0;
 	deadline = document.getElementById('deadline').value - 0;
+	prioridade = document.getElementById('prioridade').value - 0;
 	pids += 1;
 
-	var processo = new Processo(pids, tempochegada, tempoexecucao, deadline);
+	var processo = new Processo(pids, tempochegada, tempoexecucao, deadline, prioridade);
 	processos.push(processo);
 	console.log(processos);
+
+	criarprocessofront();
+
+	function criarprocessofront(){
+		document.getElementById('tabelaprocessos').innerHTML += '<tr><td headers="tdpid">' + pids + '</td><td headers="tdtc">' + tempochegada + '</td><td headers="tdte">' + tempoexecucao + '</td><td headers="tdd">' + deadline + '</td><td headers="tdp">' + prioridade + '</td></tr><br>';
+	}
 }
 
-function SJF(){processos.sort(function(a, b){
+function SJF(){
+
+	processos.sort(function(a, b){
 		if(a.tempochegada!=b.tempochegada){
 			return a.tempochegada - b.tempochegada;
 		}
@@ -34,10 +45,9 @@ function SJF(){processos.sort(function(a, b){
 		tempo += processos[i].tempoexecucao;
 		processos[i].turnaround = tempo - processos[i].tempochegada;
 		turnaroundmedio += processos[i].turnaround;
-		console.log(processos[i].turnaround);
 	}
 	turnaroundmedio /= processos.length;
-	console.log(turnaroundmedio);
+	console.log("Turnaround médio: ",turnaroundmedio);
 	console.log(processos);
 
 }
@@ -59,15 +69,15 @@ function FIFO(){
 	processos.sort(function(a, b){
 		return a.tempochegada - b.tempochegada;
 	})
+
 	tempo = processos[0].tempochegada;
 	for (var i = 0; i < processos.length; i++) {
 		tempo += processos[i].tempoexecucao;
 		processos[i].turnaround = tempo - processos[i].tempochegada;
 		turnaroundmedio += processos[i].turnaround;
-		console.log(processos[i].turnaround);
 	}
 	turnaroundmedio /= processos.length;
-	console.log(turnaroundmedio);
+	console.log("Turnaround médio :", turnaroundmedio);
 	console.log(processos);
 }
 
@@ -75,6 +85,21 @@ function RR(){
 	processos.sort(function(a, b){ // fifo
 		return a.tempochegada - b.tempochegada;
 	})
+
+	console.log(processos);
+
+	preempcao();
+}
+
+function Prioridade(){
+	processos.sort(function(a, b){
+		if(a.tempochegada!=b.tempochegada){
+			return a.tempochegada - b.tempochegada;
+		}
+		return a.prioridade - b.prioridade;
+	})
+
+	console.log(processos);
 
 	preempcao();
 }
@@ -209,3 +234,149 @@ function RR(){
 	}
 }
 */
+
+function savechanges(){
+	document.getElementById('infoquantum').innerHTML = document.getElementById('quantum').value;
+	document.getElementById('infosobrecarga').innerHTML = document.getElementById('sobrecarga').value;
+
+	if (escalonamento == 1) { document.getElementById('infoescalonamento').innerHTML = "SJF";}
+	else if (escalonamento == 2) { document.getElementById('infoescalonamento').innerHTML = "EDF";}
+	else if (escalonamento == 3) { document.getElementById('infoescalonamento').innerHTML = "FIFO";}
+	else if (escalonamento == 4) { document.getElementById('infoescalonamento').innerHTML = "RR";}
+	else { document.getElementById('infoescalonamento').innerHTML = "Prioridade";}
+}
+
+function selecionado(id){
+	document.getElementById(id).style.backgroundColor = "white";
+	document.getElementById(id).style.color = "#66CDAA";
+
+	if (id == 'sjfbtn') {
+		document.getElementById('edfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('edfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('fifobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('fifobtn').style.color = "#F0F8FF";
+
+		document.getElementById('rrbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('rrbtn').style.color = "#F0F8FF";
+
+		document.getElementById('priobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('priobtn').style.color = "#F0F8FF";
+
+		escalonamento = 1;
+	}
+	else if (id == 'edfbtn') {
+		document.getElementById('sjfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('sjfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('fifobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('fifobtn').style.color = "#F0F8FF";
+
+		document.getElementById('rrbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('rrbtn').style.color = "#F0F8FF";
+
+		document.getElementById('priobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('priobtn').style.color = "#F0F8FF";
+
+		escalonamento = 2;
+		
+	}
+	else if (id == 'fifobtn') {
+		document.getElementById('sjfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('sjfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('edfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('edfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('rrbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('rrbtn').style.color = "#F0F8FF";
+
+		document.getElementById('priobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('priobtn').style.color = "#F0F8FF";
+		
+		escalonamento = 3;
+	}
+	else if (id == 'rrbtn') {
+		document.getElementById('sjfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('sjfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('edfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('edfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('fifobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('fifobtn').style.color = "#F0F8FF";
+
+		document.getElementById('priobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('priobtn').style.color = "#F0F8FF";
+		
+		escalonamento = 4;
+	}
+	else {
+		document.getElementById('sjfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('sjfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('edfbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('edfbtn').style.color = "#F0F8FF";
+
+		document.getElementById('rrbtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('rrbtn').style.color = "#F0F8FF";
+
+		document.getElementById('fifobtn').style.backgroundColor = "#66CDAA";
+		document.getElementById('fifobtn').style.color = "#F0F8FF";
+		
+		escalonamento = 5;
+
+	}
+}
+
+function rodarescalonamento(){
+	if (escalonamento == 0) {
+		alert("Selecione um tipo de escalonamento");
+	}
+	else if (processos.length == 0) {
+		alert("Crie processos");
+	}
+	else {
+		if (escalonamento == 1) {
+			SJF();
+			np();
+		}
+
+		else if (escalonamento == 2) {
+
+		}
+		else if (escalonamento == 3) {
+			FIFO();
+			np();
+		}
+
+		function np(){
+			aux = 0;
+			prontos = [];
+			setInterval(nptimer, 1000);	
+		}	
+	}
+}
+
+function nptimer(){
+	for (var i = aux; i < processos.length; i++) {
+		if(processos[i].tempochegada = tempo){
+			prontos.push(processos[i]);
+		}
+	}
+	aux = i;
+	if(prontos.length > 0){
+		document.getElementById('tdcpu').innerHTML = prontos[0].pid;
+		document.getElementById('tdfilaprontos').innerHTML = '';
+		for (var i = 1; i < prontos.length; i++) {
+			document.getElementById('tdfilaprontos').innerHTML += prontos[i].pid + ' ';
+		}
+		prontos.splice(0, 1);
+	}
+	else{
+		document.getElementById('tdcpu').innerHTML = '';
+		document.getElementById('tdtam').innerHTML = turnaroundmedio;
+	}
+	tempo++; 
+	document.getElementById('infotempo').innerHTML = tempo;
+}
